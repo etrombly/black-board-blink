201d0e70493f8f1751f97dec4a743ec58b309e95
Eric Trombly <etrombly@yahoo.com>
2019-03-20 09:02:52 -0500

    initial working version

--- src/main.rs
+++ src/main.rs
@@ -9,35 +9,30 @@ use cortex_m::peripheral::syst::SystClkSource;
 use cortex_m_rt::entry;
 extern crate stm32f4;
 use stm32f4::stm32f407;
+use stm32f4xx_hal::{
+    delay::Delay,
+    prelude::*,
+};
 
 #[entry]
 fn main() -> ! {
-    let mut peripherals = stm32f407::Peripherals::take().unwrap();
+    let peripherals = stm32f407::Peripherals::take().unwrap();
+    let rcc = peripherals.RCC.constrain();
+    let clocks = rcc
+            .cfgr
+            .sysclk(64.mhz())
+            .pclk1(32.mhz())
+            .freeze();
     
-    //let mut core = stm32f407::CorePeripherals::take().unwrap();
-    let gpioa = &peripherals.GPIOA;
-    //let rcc = &peripherals.RCC;
-    //let flash = &peripherals.FLASH;
-
-    // TODO: test performance and see what this needs to actually be set to
-    //let clocks = rcc
-    //    .cfgr
-    //    .sysclk(64.mhz())
-    //    .pclk1(32.mhz())
-    //    .freeze(&mut flash.acr);
-
-
-    // configure the system timer
-    // TODO: test performance and see what this needs to actually be set to
-    //core.SYST.set_clock_source(SystClkSource::Core);
-    //core.SYST.set_reload(100_000);
-    //core.SYST.enable_interrupt();
-    //core.SYST.enable_counter();
-
-
-    gpioa.odr.modify(|_, w| w.odr6().set_bit());
+    let core = stm32f407::CorePeripherals::take().unwrap();
+    let gpioa = peripherals.GPIOA.split();
+    let mut led = gpioa.pa6.into_push_pull_output();
+    let mut delay = Delay::new(core.SYST, clocks);
 
     loop {
-        
+        led.set_low();
+        delay.delay_ms(100_u8);
+        led.set_high();
+        delay.delay_ms(100_u8);
     }
 }
